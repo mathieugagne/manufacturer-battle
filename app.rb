37fe2190813f0bcb500b1c8d036bd03a7f784fe8
Mathieu Gagne <gagne.mathieu@hotmail.com>
2015-09-19 18:33:13 -0400

    Removes /chart

--- app.rb
+++ app.rb
@@ -9,12 +9,6 @@ module ManufacturerBattle
       erb :"index.html"
     end
 
-    get "/chart" do
-      user_agent = UserAgent.parse(request.user_agent)
-      @platform = user_agent.platform
-      erb :"chart.html"
-    end
-
     get "/assets/js/application.js" do
       content_type :js
       @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
